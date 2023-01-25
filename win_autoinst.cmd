7ead0e2c5c30a4eed625f55e3b7e45459b08ac15
Kyoudai Ken <bloodysword@gmx.net>
2023-01-25 14:55:22 +0100

    Script to install Windows
    Allows you to install Windows from the install.esd or install.wim without using the setup wizard. It does NOT skip OOBE!

---
+++ win_autoinst.cmd
@@ -0,0 +1,20 @@
+@echo off
+echo --> Partitioning and formatting disk %1, Win partition size %2 MB
+pause
+(echo sel dis %1
+echo conv gpt
+echo cre par efi size 512
+echo form fs fat32 quick
+echo ass letter w
+echo cre par pri size %2
+echo form quick
+echo ass letter z
+) | diskpart
+echo --> Installing Windows from install image %3
+dism /apply-image /imagefile:%3 /index:1 /applydir:z:\
+echo --> Installing boot environment
+bcdboot z:\windows /s w:
+echo Before we reboot, check the backlog for errors and
+pause
+echo --> Rebooting
+wpeutil reboot

22cf727c38612eca2ba32d638732b272c645fbb4
Kyoudai Ken <bloodysword@gmx.net>
2023-01-25 15:09:17 +0100

    win_autoinst: Made the partition size parameter optional

--- win_autoinst.cmd
+++ win_autoinst.cmd
@@ -1,4 +1,5 @@
 @echo off
+IF NOT DEFINED %2 ( set wsize= ) ELSE ( set wsize=size %2 )
 echo --> Partitioning and formatting disk %1, Win partition size %2 MB
 pause
 (echo sel dis %1
@@ -6,7 +7,7 @@ echo conv gpt
 echo cre par efi size 512
 echo form fs fat32 quick
 echo ass letter w
-echo cre par pri size %2
+echo cre par pri %wsize%
 echo form quick
 echo ass letter z
 ) | diskpart
